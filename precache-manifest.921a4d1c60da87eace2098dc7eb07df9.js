fdbb61141699b149f1c1af453ace08ee8b316c39
psycho <viswassaripalli@viswass-MacBook-Air.local>
2019-05-24 19:41:40 +0530

    Updates

---
+++ precache-manifest.921a4d1c60da87eace2098dc7eb07df9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a28b22ab4ce7769a19620a476b8ece1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa6fa6d19b9de9d58742",
+    "url": "/static/css/main.1b0eb687.chunk.css"
+  },
+  {
+    "revision": "17668d01afca0d313a68",
+    "url": "/static/js/2.1ab6adf9.chunk.js"
+  },
+  {
+    "revision": "fa6fa6d19b9de9d58742",
+    "url": "/static/js/main.d065d586.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
